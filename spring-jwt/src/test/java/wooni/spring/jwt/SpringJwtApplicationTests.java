c2612e6ad4cddbeffaa826522e7543872e80a048
woo_ooni <heeey@nate.com>
2024-10-04 11:18:13 +0900

    create spring-jwt project

---
+++ spring-jwt/src/test/java/wooni/spring/jwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package wooni.spring.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
